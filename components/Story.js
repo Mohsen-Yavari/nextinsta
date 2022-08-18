1c73254c5991c2f363f3c8dba777f53e7e954b05
Mohsen-Yavari <mohsenyavari713@gmail.com>
2022-08-18 10:07:56 +0330

    style the story section and  install talwind scrollbar

--- components/Story.js
+++ components/Story.js
@@ -2,9 +2,15 @@
 import {PlusIcon} from "@heroicons/react/solid";
 export default function Story({img,username,isUser}) {
   return (
-    <div>
-        <img src={img} alt={username} />
-        <p>{username}</p>
+    <div className="">
+        <img 
+        src={img} 
+        alt={username} 
+        className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer 
+        hover:scale-110 transition-transform duration-200 ease-out"
+        />
+        <p className="text-xs w-14 truncate"
+        >{username}</p>
     </div>
   )
 }
